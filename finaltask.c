e5da5070e5a68f4b85cf416b55930b38ad3713e9
Abdo Adel <bidoadel@gmail.com>
2023-10-13 18:34:16 +0300

    file added

--- finaltask.c
+++ finaltask.c
@@ -2,21 +2,21 @@
 #include <string.h>
 
 typedef struct {
-    char username[30];
-    char password[30];
+    char username[10];
+    char password[10];
 } User;
 
-#define MAX_USERS 100
+#define max 10
 
-User users[MAX_USERS];
-int userCount = 0;
-
+User users[max];
+int count = 0;
+// max user counter 
 void registerUser() {
-    if (userCount == MAX_USERS) {
-        printf("Maximum user limit exceeded. Cannot register more users.\n");
+    if (count == max) {
+        printf("You reach the max user count.\n");
         return;
     }
-
+// start regesteration code 
     User newUser;
     printf("Enter username: ");
     scanf("%s", newUser.username);
@@ -24,47 +24,47 @@ void registerUser() {
     printf("Enter password: ");
     scanf("%s", newUser.password);
 
-    users[userCount++] = newUser;
+    users[count++] = newUser;
     printf("User registered successfully!\n");
 }
 
 void loginUser() {
-    char username[30], password[30];
+    char username[10], password[10];
     printf("Enter username: ");
     scanf("%s", username);
 
     printf("Enter password: ");
     scanf("%s", password);
-int i=0
-    for ( i;i < userCount; i++) {
+int i=0;
+    for ( i;i < count; i++) {
         if (strcmp(users[i].username, username) == 0 && strcmp(users[i].password, password) == 0) {
             printf("User logged in successfully!\n");
             return;
         }
     }
 
-    printf("Invalid username or password. Please try again.\n");
+    printf("Invalid username or password. Regest again.\n");
 }
 
 int main() {
-    int option;
+    int state ;
 
     while (1) {
-        printf("Press '1' to Register \nPress '2' to Login \nPress '3' to Exit\n\n");
-        scanf("%d", &option);
+        printf("to Regist press '1'  \nto login press '2' \nto leave press '3'\n\n");
+        scanf("%d", &state );
 
-        if (option == 1) {
+        if (state  == 1) {
             registerUser();
-        } else if (option == 2) {
+        } else if (state  == 2) {
             loginUser();
-        } else if (option == 3) {
-            printf("Program terminated.\n");
+        } else if (state  == 3) {
+            printf("ُExit program.\n");
             break;
         } else {
-            printf("Invalid option. Please try again.\n");
+            printf("un defined state try again.\n");
         }
 
-        printf("\n");
+        printf("\n\n");
     }
 
     return 0;
